a62401c0cb15804ac0356a9c9ce3115b52bfea25
PiggyPiglet <sam@thekeilars.com>
2017-04-05 18:08:16 +0800

    bans

--- bans.php
+++ bans.php
@@ -8,7 +8,7 @@
 <?php
    // <<-----------------Database Connection------------>> //
    require 'includes/data/database.php';
-   $sql = 'SELECT name, reason, banner, time, expires FROM bans ORDER BY time DESC LIMIT 20';
+   $sql = 'SELECT name, reason, operator, punishmentType, start, end FROM Punishments ORDER BY time DESC LIMIT 20';
    $retval = $conn->query($sql);
    ?>
 <body>
@@ -38,24 +38,24 @@
             </thead>
             <tbody>
                <?php while($row = $retval->fetch_assoc()) { 
-                  if($row['banner'] == null) {
-                     $row['banner'] = 'Console';
+                  if($row['operator'] == null) {
+                     $row['operator'] = 'Console';
                   }
                   // <<-----------------Ban Date Converter------------>> //
-                  $timeEpoch = $row['time'];
+                  $timeEpoch = $row['end'];
                   $timeConvert = $timeEpoch / 1000;
                   $timeResult = date('F j, Y, g:i a', $timeConvert);
                   // <<-----------------Expiration Time Converter------------>> //
-                  $expiresEpoch = $row['expires'];
+                  $expiresEpoch = $row['end'];
                   $expiresConvert = $expiresEpoch / 1000;
                   $expiresResult = date('F j, Y, g:i a', $expiresConvert);
                   ?>
                <tr>
                   <td><?php echo "<img src='https://mcapi.ca/avatar/2d/" . $row['name'] . "/25' style='margin-bottom:5px;margin-right:5px;border-radius:2px;' />" . $row['name'];?></td>
-                  <td><?php echo "<img src='https://mcapi.ca/avatar/2d/" . $row['banner'] . "/25'  style='margin-bottom:5px;margin-right:5px;border-radius:2px;' />" . $row['banner'];?></td>
+                  <td><?php echo "<img src='https://mcapi.ca/avatar/2d/" . $row['operator'] . "/25'  style='margin-bottom:5px;margin-right:5px;border-radius:2px;' />" . $row['operator'];?></td>
                   <td style="width: 30%;"><?php echo $row['reason'];?></td>
                   <td><?php echo $timeResult;?></td>
-                  <td><?php if($row['expires'] == 0) {
+                  <td><?php if($row['end'] == -1) {
                      echo 'Permanent Ban';
                      } else {
                      echo $expiresResult; }?></td>
