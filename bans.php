6e0922100088b0747c7dbcc9099cc29ef620b4d8
PiggyPiglet <sam@thekeilars.com>
2017-04-12 20:16:41 +0800

    Bans fix
    Thanks andrew

--- bans.php
+++ bans.php
@@ -1,14 +1,14 @@
 <?php
-   include 'includes/head.php';
-   include 'includes/header.php';
-   ?>
+include 'includes/head.php';
+include 'includes/header.php';
+?>
 <head>
-   <title>Bans/Tempbans - <?php echo $name; ?></title>
+    <title>Bans/Tempbans - <?php echo $name; ?></title>
 </head>
 <?php
-   // <<-----------------Database Connection------------>> //
-   require 'includes/data/database.php';
-   $sql = 'SELECT name, reason, operator, punishmentType, start, end FROM Punishments ORDER BY start DESC LIMIT 20';
+// <<-----------------Database Connection------------>> //
+require 'includes/data/database.php';
+$sql = 'SELECT * FROM Punishments WHERE punishmentType = "BAN" || "TEMP_BAN" ORDER BY start DESC LIMIT 20;';
 $retval = $conn->query($sql);
 ?>
 <body>
@@ -38,72 +38,54 @@ $retval = $conn->query($sql);
                 </thead>
                 <tbody>
                 <?php while($row = $retval->fetch_assoc()) {
-                    if($row['banner'] == null) {
-                        $row['banner'] = 'Console';
+                    if($row['operator'] == null) {
+                        $row['operator'] = 'Console';
                     }
                     // <<-----------------Ban Date Converter------------>> //
-                    $timeEpoch = $row['time'];
+                    $timeEpoch = $row['start'];
                     $timeConvert = $timeEpoch / 1000;
                     $timeResult = date('F j, Y, g:i a', $timeConvert);
                     // <<-----------------Expiration Time Converter------------>> //
-                    $expiresEpoch = $row['expires'];
+                    $expiresEpoch = $row['end'];
                     $expiresConvert = $expiresEpoch / 1000;
                     $expiresResult = date('F j, Y, g:i a', $expiresConvert);
                     ?>
                     <tr>
                         <td>
                             <?php
-                            if ($row['punishmentType'] == 'BAN' || 'TEMP_BAN') {
-                                echo "<img src='https://mcapi.ca/avatar/2d/" . $row['name'] . "/25' style='margin-bottom:5px;margin-right:5px;border-radius:2px;' />" . $row['name'];
-                            } else {
-                                echo "error";
-                            }
+                            echo "<img src='https://mcapi.ca/avatar/2d/" . $row['name'] . "/25' style='margin-bottom:5px;margin-right:5px;border-radius:2px;' />" . $row['name'];
                             ?></td>
                         <td>
                             <?php
-                            if($row['punishmentType'] == 'BAN' || 'TEMP_BAN') {
-                                echo "<img src='https://mcapi.ca/avatar/2d/" . $row['operator'] . "/25'  style='margin-bottom:5px;margin-right:5px;border-radius:2px;' />" . $row['operator'];
-                            } else {
-                                echo "error";
-                            }
+                            echo "<img src='https://mcapi.ca/avatar/2d/" . $row['operator'] . "/25'  style='margin-bottom:5px;margin-right:5px;border-radius:2px;' />" . $row['operator'];
                             ?>
                         </td>
                         <td style="width: 30%;">
+                            <?php echo $row['reason'];?>
+                        </td>
+                        <td>
                             <?php
-                            if($row['punishmentType'] == 'BAN' || 'TEMP_BAN') {
-                                echo $row['reason'];
-                            } else {
-                                echo "error";
-                            }
+                            echo $timeResult;
                             ?>
                         </td>
                         <td>
                             <?php
-                            if($row['punishmentType'] == 'BAN' || 'TEMP_BAN') {
-                                echo $timeResult;
+
+                            if($row['end'] == 0) {
+                                echo 'Permanent Ban';
                             } else {
-                                echo "error";
+                                echo $expiresResult;
                             }
-                            ?>
-                        </td>
-                        <td>
-                            <?php
-                            if($row['punishmentType'] == 'BAN' || 'TEMP_BAN') {
-                                if($row['end'] == 0) {
-                                    echo 'Permanent Ban';
-                                } else {
-                                    echo $expiresResult;
-                                }
-                            } else {
-                                echo "error";
-                            }
+
                             ?>
                         </td>
                     </tr>
-                <?php }
+                    <?php
+                }
                 $conn->close();
-                echo "</tbody></table>";
                 ?>
+                </tbody>
+            </table>
         </div>
     </div>
     <?php include 'includes/footer.php'; ?>
